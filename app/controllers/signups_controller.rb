3b83b2201c51e1d343d5c693207b663afbe84d99
codecademy <ccuser@codecademy.com>
2016-12-12 17:22:56 -0500

    images choosen, moving to routing setup/forums/users

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,6 +1,20 @@
 class SignupsController < ApplicationController
   
   def new
+    @signup = Signup.new()
   end
   
+  def create
+    @signup = Signup.new(signup_params)
+    if
+      
+    else
+      
+    end
+  end
+  
+  private
+  def signup_params
+    params.require.(:signup).permit(:first_name, :email)
+  end
 end
